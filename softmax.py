fdb63c9be74eca8493577e28af8d08d4af0bf27a
Sulong <88147012+Su-long-ai@users.noreply.github.com>
2024-07-19 14:49:38 +0800

    Update softmax.py

--- softmax.py
+++ softmax.py
@@ -12,8 +12,6 @@ def init_weights(m):
 batch_size = 256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 
-# PyTorch不会隐式地调整输入的形状。因此，
-# 我们在线性层前定义了展平层（flatten），来调整网络输入的形状
 net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
 
 net.apply(init_weights)
