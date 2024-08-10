afcf144d60a2b8ddb2b48aa725f72105ada4ac72
Sulong <88147012+Su-long-ai@users.noreply.github.com>
2024-08-10 15:13:56 +0800

    Add files via upload

---
+++ 经典神经网络模型/NiN.py
@@ -0,0 +1,47 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+import matplotlib.pyplot as plt
+import os
+
+# 我的程序中链接了太多个OpenMP运行时库副本，此处作为临时解决方案，不推荐
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
+def nin_block(in_channels, out_channels, kernel_size, strides, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1),
+        nn.ReLU()
+    )
+
+
+net = nn.Sequential(
+    nin_block(1, 96, kernel_size=11, strides=4, padding=0),
+    nn.MaxPool2d(3, stride=2),
+    nin_block(96, 256, kernel_size=5, strides=1, padding=2),
+    nn.MaxPool2d(3, stride=2),
+    nin_block(256, 384, kernel_size=3, strides=1, padding=1),
+    nn.MaxPool2d(3, stride=2),
+    nn.Dropout(0.5),
+    # 标签类别数是10
+    nin_block(384, 10, kernel_size=3, strides=1, padding=1),
+    # 自适应平均池化将特征图的尺寸调整为1x1
+    nn.AdaptiveAvgPool2d((1, 1)),
+    # 将四维的输出转成二维的输出，其形状为(批量大小,10)
+    nn.Flatten()
+)
+
+X = torch.rand(size=(1, 1, 224, 224))
+for layer in net:
+    X = layer(X)
+    print(layer.__class__.__name__, 'output shape:\t', X.shape)
+
+lr, num_epochs, batch_size = 0.1, 10, 128
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
+d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+
+plt.show()
